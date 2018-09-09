0e4d9769d8980dd217937f62b2211ec12a5c34f0
thomas-schultz@tum.de <thomas.schultz@tum.de>
2018-09-09 14:26:49 +0200

    better quoting

--- lib/user.sh
+++ lib/user.sh
@@ -4,24 +4,24 @@
 function create_server_certificate {
     name="$1" && shift
     ca="$1" && shift
-    create_user_certificate $ca $name "server_cert" $*
+    create_user_certificate "$ca" "$name" "server_cert" $*
 }
 
 function create_client_certificate {
     name="$1" && shift
     ca="$1" && shift
-    create_user_certificate $ca $name "client_cert" $*
+    create_user_certificate "$ca" "$name" "client_cert" $*
 }
 
 function create_user_certificate {
-    prepare_ca $1 && shift
+    prepare_ca "$1" && shift
     name="$1" && shift
     extension="$1" && shift
 
     mkdir -p "$ca_dir/user_certs/$name/"
 
     prompt "creating private key for $name"
-    openssl genrsa -out $ca_dir/user_certs/$name/key.pem $keylength
+    openssl genrsa -out "$ca_dir/user_certs/$name/key.pem" $keylength
     cont $?
 
     tmp_cnf="$ca_dir/user_certs/$name/cert.cnf"
@@ -30,48 +30,49 @@ function create_user_certificate {
 
     prompt "creating certificate request for '$name'"
     puts "openssl req $batch_mode -config $tmp_cnf -key $ca_dir/user_certs/$name/key.pem $passin -new -out $ca_dir/csr/$name-csr.pem"
-    openssl req $batch_mode -config $tmp_cnf -key $ca_dir/user_certs/$name/key.pem $passin -new -out $ca_dir/csr/$name-csr.pem
+    openssl req $batch_mode -config "$tmp_cnf" -key "$ca_dir/user_certs/$name/key.pem" $passin -new -out "$ca_dir/csr/$name-csr.pem"
     cont $?
 
-    openssl req -in $ca_dir/csr/$name-csr.pem -text -noout > $ca_dir/csr/$name-csr.txt
+    openssl req -in "$ca_dir/csr/$name-csr.pem" -text -noout > "$ca_dir/csr/$name-csr.txt"
     puts "$ca_dir/csr/$name-csr.txt"
-    rm $tmp_cnf
+    rm "$tmp_cnf"
 
     prompt "extracting subjectAltNames from CSR for $name"
     extract_san_from_csr "$tmp_cnf" "$ca_dir/csr/$name-csr.txt"
 
     prompt "signing server certificate for $name with CA '$ca_dir'"
     puts "openssl ca $batch_mode -config $tmp_cnf -extensions $extension $passin -days $cert_days -notext -in $ca_dir/csr/$name-csr.pem -out $ca_dir/user_certs/$name/cert.pem"
-    openssl ca $batch_mode -config $tmp_cnf -extensions $extension $passin -days $cert_days -notext -in $ca_dir/csr/$name-csr.pem -out $ca_dir/user_certs/$name/cert.pem
+    openssl ca $batch_mode -config "$tmp_cnf" -extensions $extension $passin -days $cert_days -notext -in "$ca_dir/csr/$name-csr.pem" -out "$ca_dir/user_certs/$name/cert.pem"
     cont $?
 
-    openssl x509 -noout -text -in $ca_dir/user_certs/$name/cert.pem > $ca_dir/user_certs/$name/cert.txt
+    openssl x509 -noout -text -in "$ca_dir/user_certs/$name/cert.pem" > "$ca_dir/user_certs/$name/cert.txt"
     puts "$ca_dir/user_certs/$name/cert.txt"
 
-    chmod 400 $ca_dir/user_certs/$name/key.pem
-    rm $tmp_cnf
+    chmod 400 "$ca_dir/user_certs/$name/key.pem"
+    rm "$tmp_cnf"
 
-    if [ -e $ca_dir/certs/chain.pem ]; then
-        cp $ca_dir/certs/chain.pem $ca_dir/user_certs/$name/chain.pem
+    if [ -e "$ca_dir/certs/chain.pem" ]; then
+        cp "$ca_dir/certs/chain.pem" "$ca_dir/user_certs/$name/chain.pem"
     else
-        cp $ca_dir/certs/cert.pem $ca_dir/user_certs/$name/chain.pem
+        cp "$ca_dir/certs/cert.pem" "$ca_dir/user_certs/$name/chain.pem"
     fi
-
+    cp "$ca_dir/certs/ca.*.pem" "$ca_dir/user_certs/$name/"
+    
     convert_certs "$ca_dir/user_certs/$name"
-    [ -z $pkcs12 ] || export_pkcs12 "$ca_dir" "$name"
+    [ -z "$pkcs12" ] || export_pkcs12 "$ca_dir" "$name"
 }
 
 
 function revoke_user_certificate {
-    use_ca $1 && shift
+    use_ca "$1" && shift
     name="$1" && shift
 
     puts "$ca_dir/user_certs/$name/cert.pem"
 
     prompt "revoking server certificate for $name"
-    if [ -e $ca_dir/user_certs/$name/cert.pem ]; then
+    if [ -e "$ca_dir/user_certs/$name/cert.pem" ]; then
         cert="$ca_dir/user_certs/$name/cert.pem"
-    elif [ -e $ca_dir/newcerts/$name.pem ]; then
+    elif [ -e "$ca_dir/newcerts/$name.pem" ]; then
         cert="$ca_dir/newcerts/$name.pem"
     else
         echo "ERROR in revoke_user_certificate(): certificate '$name' not found!"
@@ -79,7 +80,7 @@ function revoke_user_certificate {
     fi
     echo $cert
     puts "openssl ca $batch_mode -config $ca_cnf $passin -revoke $cert"
-    openssl ca $batch_mode -config $ca_cnf $passin -revoke $cert
+    openssl ca $batch_mode -config "$ca_cnf" $passin -revoke "$cert"
     cont $?
 
     passedout="$passin" # hack to pass passphrase
@@ -87,15 +88,15 @@ function revoke_user_certificate {
 }
 
 function export_pkcs12 {
-    use_ca $1 && shift
+    use_ca "$1" && shift
     name="$1" && shift
 
     prompt "exporting to pkcs12 format"
     puts "openssl pkcs12 -export $pkcs12_passout -inkey $ca_dir/user_certs/$name/key.pem -in $ca_dir/user_certs/$name/cert.pem -certfile $ca_dir/user_certs/$name/chain.pem -out $ca_dir/user_certs/$name.p12"
-    openssl pkcs12 -export $pkcs12_passout -inkey $ca_dir/user_certs/$name/key.pem -in $ca_dir/user_certs/$name/cert.pem -certfile $ca_dir/user_certs/$name/chain.pem -out $ca_dir/user_certs/$name.p12
+    openssl pkcs12 -export $pkcs12_passout -inkey "$ca_dir/user_certs/$name/key.pem" -in "$ca_dir/user_certs/$name/cert.pem" -certfile "$ca_dir/user_certs/$name/chain.pem" -out "$ca_dir/user_certs/$name.p12"
     cont $?
 
-    echo "$pkcs12_passout" | sed 's/-passout pass://g' > $ca_dir/user_certs/$name/export.pw
+    echo "$pkcs12_passout" | sed 's/-passout pass://g' > "$ca_dir/user_certs/$name/export.pw"
     cont $?
     puts "$ca_dir/user_certs/$name.p12"
 }
